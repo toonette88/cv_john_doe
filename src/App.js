eb39b4a2952df6ae1ae343487cd411566da71b24
Amélie <amelie.adele@sfr.fr>
2024-04-27 18:16:03 +0200

    Ajout de la barre de navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-
+import Navbar from "./Components/Navbar";
 import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Realisations from "./pages/Realisations";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Mentions from "./pages/Mentions";
+
 
 export default function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/realisations" element={<Realisations />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/mentions" element={<Mentions />} />
       </Routes>
     </div>
   );
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import User from "../Components/User";
+
+const NavBar = () => {
+  return (
+    <header>
+      <nav
+        class="navbar navbar-dark navbar-expand-lg bg-dark border-bottom border-body text-light text-uppercase fixed-top "
+        id="mainNav"
+      >
+        <div class="container-fluid">
+          <Link className="navbar-brand text-light text-uppercase" to="/">
+            {User.firstName} {User.lastName}
+          </Link>
+          <button
+            className="navbar-toggler text-uppercase text-light rounded"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon navbar-dark"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarResponsive">
+            <ul className="navbar-nav text-uppercase text-start m-2">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Accueil
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/services">
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/realisations">
+                  Réalisations
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/blog">
+                  Blog
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">
+                  Me contacter
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
+
