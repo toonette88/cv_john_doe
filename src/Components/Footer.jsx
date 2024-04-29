e70f7c5a0898f8613eabb226dc031950c399b6cc
Amélie <amelie.adele@sfr.fr>
2024-04-29 22:30:31 +0200

    Modification de polices et couleurs de fond

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "../assets/styles/home.css";
 import User from "../Components/User";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
